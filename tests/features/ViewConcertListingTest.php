d8bea31091c4870887e84d06e14cb5a16f75560b
Jeandre Rohlandt <jeandre@topwebdesign.co.za>
2018-01-02 17:30:57 +0200

    start concert model and tests

---
+++ tests/features/ViewConcertListingTest.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Carbon\Carbon;
+use App\Concert;
+
+class ViewConcertListingTest extends TestCase
+{
+    /** @test */
+    function user_can_view_a_concert_listing()
+    {
+        // Arrange
+        // Create a concert
+        $concert = Concert::create([
+            'title' => 'The Red Chord',
+            'subtitle' => 'with Animosity and Lethargy',
+            'date' => Carbon::parse('January 13, 2018 8:00pm'),
+            'ticket_price' => 3250,
+            'venue' => 'The Mosh Pit',
+            'venue_address' => '123 Example Lane',
+            'city' => 'Laraville',
+            'state' => 'ON',
+            'zip' => '17916',
+            'additional_information' => 'For tickets, call (555) 555-5555.',
+        ]);
+
+        // Act
+        // View the concert listing
+        $this->visit('/concerts/'.$concert->id);
+
+        // Assert
+        // See the concert details
+        $this->see('The Red Cord');
+        $this->see('with Animosity and Lethargy');
+        $this->see('January 13, 2018');
+        $this->see('8:00');
+        $this->see('32.50');
+        $this->see('The Mosh Pit');
+        $this->see('123 Example Lane');
+        $this->see('Laraville, ON 17916');
+        $this->see('For tickets, call (555) 555-5555.');
+    }
+}
