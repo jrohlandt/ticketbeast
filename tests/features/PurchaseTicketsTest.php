2877619badc37b013eaa75d0eac7e65d551c5cc9
Jeandre Rohlandt <jeandre@topwebdesign.co.za>
2018-01-30 09:55:36 +0200

    add method to FakePaymentGateway to test for race condition when purchasing tickets

--- tests/features/PurchaseTicketsTest.php
+++ tests/features/PurchaseTicketsTest.php
@@ -126,6 +126,38 @@ class PurchaseTicketsTest extends TestCase {
     }
 
     /** @test */
+    function cannot_purchase_tickets_another_customer_is_already_trying_to_purchase()
+    {
+        $this->disableExceptionHandling();
+
+        $concert = factory(Concert::class)->states('published')->create(['ticket_price' => 1200])->addTickets(3);
+
+        $this->paymentGateway->beforeFirstCharge(function($paymentGateway) use ($concert) {
+            $this->orderTickets($concert, [
+                'email' => 'personB@example.com',
+                'ticket_quantity' => 1,
+                'payment_token' => $paymentGateway->getValidTestToken(),
+            ]);
+
+            $this->assertResponseStatus(422);
+            $order = $concert->orders()->where('email', 'personB@example.com')->first();
+            $this->assertNull($order);
+            $this->assertEquals(0, $paymentGateway->totalCharges());
+        });
+
+        $this->orderTickets($concert, [
+            'email' => 'personA@example.com',
+            'ticket_quantity' => 3,
+            'payment_token' => $this->paymentGateway->getValidTestToken(),
+        ]);
+
+        $this->assertEquals(3600, $this->paymentGateway->totalCharges());
+        $order = $concert->orders()->where('email', 'personA@example.com')->first();
+        $this->assertNotNull($order);
+        $this->assertEquals(3, $order->tickets()->count());
+    }
+
+    /** @test */
     function email_is_required_to_purchase_tickets()
     {
         $concert = factory(Concert::class)->states('published')->create();
